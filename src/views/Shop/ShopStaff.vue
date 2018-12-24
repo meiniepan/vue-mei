bf90c961c67f3bbf4665e8deeb2a1f6ddf05c303
liulinnan <liulinnan@ulaidao.com>
2018-12-24 17:12:25 +0800

    Add common styles
    Add table width, pop-up window, etc.
    
    Signed-off-by: liulinnan@ulaidao.com

--- src/views/Shop/ShopStaff.vue
+++ src/views/Shop/ShopStaff.vue
@@ -79,7 +79,7 @@
         },
         methods: {
             deleteAdmin(row) {
-                console.log(row);
+                //console.log(row);
                 this.$confirm('确定要删除管理员?', {
                     confirmButtonText: '确定',
                     cancelButtonText: '取消',
