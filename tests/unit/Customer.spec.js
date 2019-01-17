f34d7bcc94222efa9888c05d9ccea343adb1ed37
huangpianpian <huangpianpian@ulaidao.com>
2019-01-17 18:23:08 +0800

    unitTest and Configuration submit interface debugging
    unitTest and Configuration submit interface debugging
    
    Signed-off-by: huangpianpian@ulaidao.com

---
+++ tests/unit/Customer.spec.js
@@ -0,0 +1,8 @@
+import  Vue from 'vue'
+import Customer from '@/views/Customer/Customer'
+import { mount, createLocalVue, shallowMount } from "@vue/test-utils"
+describe('Customer.vue', () => {
+    it('has a created hook', () => {
+    })
+
+})
---
+++ tests/unit/Mechanic.spec.js
@@ -0,0 +1,8 @@
+import  Vue from 'vue'
+import Mechanic from '@/views/Mechanic/Mechanic'
+import { mount, createLocalVue, shallowMount } from "@vue/test-utils"
+describe('Mechanic.vue', () => {
+    it('has a created hook', () => {
+    })
+
+})
